974ae130b4d3317797733aa1d7920df96aba4ccb
Dev_sharma52 <devsh0524@gmail.com>
2026-06-24 15:30:39 +0530

    Day3 done

---
+++ Day3/ques2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int start, end;
+    cout << "Enter starting number:";
+    cin >> start;
+    
+    cout << "Enter ending number:";
+    cin >> end;
+    cout << "Prime numbers between" << start << " and " << end << " are:\n";
+    for (int num = start; num <= end; num++) {
+        bool is prime = true;
+        if (num <= 1)
+        isPrime = false;
+        else {
+            for (int i = 2; i <= num / 2; i++){
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime)
+        cout << num << " ";
+    }
+    return 0;
+}

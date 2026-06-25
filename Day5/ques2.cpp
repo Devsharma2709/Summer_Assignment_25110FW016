02808bcdaf0841cd866d298645e58a4acc70242c
Dev_sharma52 <devsh0524@gmail.com>
2026-06-25 09:07:45 +0530

    Day5 done

---
+++ Day5/ques2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, temp, rem;
+    int sum = 0 ;
+    cout << "Enter a number:";
+    cin >> n;
+    temp = n;
+    while(temp > 0){
+        rem = temp % 10;
+        int fact = 1;
+        for(int i = 1; i <= rem; i++){
+            fact *= i;
+        }
+    sum += fact;
+    temp /= 10;
+    }
+if(sum == n)
+cout << n << " is a strong number";
+else
+cout << n << " is not a stong number";
+return 0;
+}

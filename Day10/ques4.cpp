050eb236bafd0378635e1e9223a80f18e76c0f9b
Dev_sharma52 <devsh0524@gmail.com>
2026-06-25 09:41:43 +0530

    Day10 done

---
+++ Day10/ques4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n= 5; for(int i = 1; i <= n; i++){
+        for (int j = 1; j <= n-i; j++){
+            cout << " ";
+        }
+        for(int j =0; j < 1; j++){
+            cout << char('A' + j);
+        }
+        for (int j = i - 2; j >= 0; j--){
+            cout << char('A'+j);
+        }
+        cout << endl;
+    }
+    return 0;
+}

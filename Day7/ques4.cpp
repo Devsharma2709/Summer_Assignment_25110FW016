7eb53874163dd55d15a7133daaf89fba613a1618
Dev_sharma52 <devsh0524@gmail.com>
2026-06-25 09:14:41 +0530

    Day7 done

---
+++ Day7/ques4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void reverseNumber(int n)
+{
+    if (n == 0)
+    return;
+    cout << n % 10;
+    reverseNumber(n / 10);
+}
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Reversed Number = ";
+    reverseNumber(n);
+    return 0;
+}
